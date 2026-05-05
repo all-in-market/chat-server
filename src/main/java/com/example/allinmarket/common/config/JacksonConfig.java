5714bb7ba1725d901586b907e14e33c7d58ef002
bjw446 <bjw446@naver.com>
2026-05-05 14:46:30 +0900

    reafactor : JacksonConfig 수정

--- src/main/java/com/example/allinmarket/common/config/JacksonConfig.java
+++ src/main/java/com/example/allinmarket/common/config/JacksonConfig.java
@@ -1,5 +1,6 @@
 package com.example.allinmarket.common.config;
 
+import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
@@ -16,6 +17,8 @@ public class JacksonConfig {
         objectMapper.registerModule(new JavaTimeModule());
 
         objectMapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
+
+        objectMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
 
         return objectMapper;
     }

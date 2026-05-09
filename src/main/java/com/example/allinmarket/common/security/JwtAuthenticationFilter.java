978b4cd0e81b3e12a0faf6163e1a28ecf774c875
LeeJun14 <777ljy777@gmail.com>
2026-05-09 16:49:08 +0900

    fix: Forbidden 검증

--- src/main/java/com/example/allinmarket/common/security/JwtAuthenticationFilter.java
+++ src/main/java/com/example/allinmarket/common/security/JwtAuthenticationFilter.java
@@ -56,6 +56,9 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
 
     private String extractToken(HttpServletRequest request) {
         String bearer = request.getHeader("Authorization");
+        if (bearer == null) {
+            bearer = request.getHeader("X-Authorization");
+        }
         if (bearer != null && bearer.startsWith("Bearer ")) {
             return bearer.substring(7);
         }
